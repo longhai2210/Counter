1d5f1b1c74b948b1fec896f901527095fda951bf
qe <qe@qes-Mac-mini.local>
2019-06-26 10:18:33 +0700

    push 2

---
+++ src/components/counter.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "http://picsum.photos/200",
+    tags: []
+  };
+
+  //constructor() {
+  //  super();
+  //  this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = () =>  {
+    this.setState({ count: this.state.count + 1})
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags .</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="" className="float-right" />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <br/>
+        {this.state.tags.length === 0 && "Create a new tag ."}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : this.state.count;
+  }
+}
+
+export default Counter;
